fe54af77871b5127f81220c3e2021216b325596d
Milena Rodrigues <rodriguesmilena.si@gmail.com>
2016-08-08 10:16:06 -0300

    Mantendo dados no formulário

--- src/main/java/com/algaworks/brewer/controller/CervejasController.java
+++ src/main/java/com/algaworks/brewer/controller/CervejasController.java
@@ -15,22 +15,21 @@ import com.algaworks.brewer.model.Cerveja;
 public class CervejasController {
 
 	public Cerveja Cerveja;
-	
+
 	@RequestMapping("/cervejas/novo")
-	public String novo(){
+	public String novo(Cerveja cerveja) {
 		return "cerveja/CadastroCerveja";
 	}
 
 	@RequestMapping(value = "/cervejas/novo", method = RequestMethod.POST)
-	public String cadastrar(@Valid Cerveja cerveja, BindingResult result, Model model, RedirectAttributes attributes){
-		if(result.hasErrors()){
-			model.addAttribute("mensagem", "Erro no formulario");
-			return "cerveja/CadastroCerveja";
-		}		
-		
+	public String cadastrar(@Valid Cerveja cerveja, BindingResult result, Model model, RedirectAttributes attributes) {
+		if (result.hasErrors()) {
+			return novo(cerveja);
+		}
+
 		attributes.addFlashAttribute("mensagem", "Cerveja salva com sucesso!");
 		System.out.println(">>SKU: " + cerveja.getSku());
 		return "redirect:/cervejas/novo";
-		
+
 	}
 }
